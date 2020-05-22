0c0a6fc3dfdc93b27d6b48f08ce57eb7070eb70a
rrsalian16 <rrsalian16@gmail.com>
2020-05-22 10:52:11 +0530

    NodeJS and ReactJs added

---
+++ React/swag-shop-web/src/services/http-services.js
@@ -0,0 +1,14 @@
+import 'whatwg-fetch';
+
+class HttpService{
+    getProducts= () =>{
+
+        fetch('http://localhost:3004/product')
+        .then(res=>{
+            console.log(res.json());
+        })
+
+    }
+}
+
+exprot default HttpService;
