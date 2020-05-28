04778e596fb5b7f9dacce4164bccd50a14ed1785
rrsalian16 <rrsalian16@gmail.com>
2020-05-28 11:37:49 +0530

    Changes

--- React-The Complete Guide Udemy/Projects/first-app/src/App.js
+++ React-The Complete Guide Udemy/Projects/first-app/src/App.js
@@ -1,103 +1,102 @@
-import React, { Component, useState } from 'react';
-import './App.css';
-import Radium, {StyleRoot} from 'radium';
-import Person from './Person/Person';
+import React, { Component, useState } from "react";
+import "./App.css";
+import Styled from "styled-components";
+import Person from "./Person/Person";
+
 
 
 /* Class Based Component */
 class App extends Component {
-
   state = {
     persons: [
-      { id:'adfsd12', name: "Rakshith", age: "23" },
-      { id:'sdf234f', name: "Rohith", age: "24" },
-      { id:'hfg234n', name: "Pratheek", age: "25" }
+      { id: "adfsd12", name: "Rakshith", age: "23" },
+      { id: "sdf234f", name: "Rohith", age: "24" },
+      { id: "hfg234n", name: "Pratheek", age: "25" },
     ],
-    showPersons: false
-  }
+    showPersons: false,
+  };
 
-  nameChangedHandler = (event,id) => {
-
-    const personIndex=this.state.persons.findIndex(p=>{ return p.id===id; })
-    const person={...this.state.persons[personIndex]};
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...this.state.persons[personIndex] };
     person.name = event.target.value;
-    const persons=[...this.state.persons];
-    persons[personIndex]=person;
-    this.setState({persons: persons })
-  }
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons });
+  };
 
   togglePersonHandler = () => {
     const doesShowPerson = this.state.showPersons;
     this.setState({
-      showPersons: !doesShowPerson
-    })
-  }
+      showPersons: !doesShowPerson,
+    });
+  };
 
-  deletePersonHandler=(personIndex)=>{
+  deletePersonHandler = (personIndex) => {
     // const persons=this.state.persons.slice()
-    const persons=[...this.state.persons];  
-    persons.splice(personIndex,1);
-    this.setState({persons:persons});
-  }
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   render() {
-
     // InlineStle :
-    const style = {
-      backgroundColor: 'blue',
-      color:'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover':{
-        backgroundColor:'lightgreen',
-        color:'black'
-      }
-    }
-
+    /* const style = {
+      backgroundColor: "blue",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
+    };
+ */
     let persons = null;
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person,index) => {
-            return <Person
-              changed={(event)=>this.nameChangedHandler(event,person.id)}
-              click={()=>this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
               />
-          
+            );
           })}
         </div>
       );
-      style.backgroundColor='red';
-      style[':hover']= {
-        backgroundColor: 'yellow',
-          color: 'black'
-      }
+      /* style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "yellow",
+        color: "black",
+      }; */
     }
 
-    let classes=[];
-    if(this.state.persons.length<=2){
+    let classes = [];
+    if (this.state.persons.length <= 2) {
       classes.push("red");
     }
-    if(this.state.persons.length<=1){
-      classes.push("bold")
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working !!!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Person</button>
+        <p className={classes.join(" ")}>This is really working !!!</p>
+        <button className='button' onClick={this.togglePersonHandler}>
+          Toggle Person
+        </button>
         {persons}
       </div>
-      </StyleRoot>
     );
 
     // Above return is conver to below return internally
@@ -105,7 +104,7 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
 
 /* Function Based Component -Functional Component */
 /* const app = props => {
--- React-The Complete Guide Udemy/Projects/first-app/src/Person/Person.js
+++ React-The Complete Guide Udemy/Projects/first-app/src/Person/Person.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 import './Person.css';
-import Radium from 'radium';
+import Styled from 'styled-components';
+
+
+const StyledDiv = Styled.div`
+            width: 60%;
+            margin: 15px auto;
+            border: 1px solid #3f3f3f;
+            box-shadow: 0px 4px 10px #333;
+            padding: 15px;
+            text-align: center;
+
+            '@media (min-width: 500px)':{
+            width:'450px'
+        }
+
+        `;
 
 /* Function Based Component */
 const person = (props) => {
 
-    const style={
+    /* const style={
         '@media (min-width: 500px)':{
             width:'450px'
         }
-    }
+    } */
 
     return (
-        <div className="Person" style={style}>
+        // <div className="Person" style={style}>
+        <StyledDiv>
             <p onClick={props.click}>My name is {props.name} & I'm {props.age} year old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyledDiv>
     );
 };
-export default Radium(person);
+export default person;
 
 /* Class Based Component */
 
