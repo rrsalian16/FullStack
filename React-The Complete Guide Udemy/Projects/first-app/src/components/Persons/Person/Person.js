565bb09fe1cc2e4b3b5efdd7dd48a62a80410148
rrsalian16 <rrsalian16@gmail.com>
2020-05-29 17:54:14 +0530

    Final commit before burger project

--- React-The Complete Guide Udemy/Projects/first-app/src/components/Persons/Person/Person.js
+++ React-The Complete Guide Udemy/Projects/first-app/src/components/Persons/Person/Person.js
@@ -1,20 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import Aux from '../../../hoc/Auxiliary'
 import classes from './Person.module.css';
 import AuthContext from '../../../context/auth-context'
 
 /* Function Based Component */
-const person = (props) => {
+const Person = (props) => {
 
-    
+    const authContext=useContext(AuthContext);
 
     console.log("[Person.js] rendering...");
     return (
         <div className={classes.Person} >
 
-            <AuthContext.Consumer>
-                {(context)=>context.authenticated ? < p > Authenticated </p> :<p>Please.. Log In</p >}
-            </AuthContext.Consumer>
+            {authContext.authenticated ? < p > Authenticated </p> :<p>Please.. Log In</p >}
 
             <p onClick={props.click}>My name is {props.name} & I'm {props.age} year old.</p>
             <p>{props.children}</p>
@@ -24,4 +22,4 @@ const person = (props) => {
     );
 };
 
-export default person;
+export default Person;
