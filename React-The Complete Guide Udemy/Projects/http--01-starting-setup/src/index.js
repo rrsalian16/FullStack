7ff21e0ecbffd9810bbdd08b93ceb8159f1c14b5
rrsalian16 <rrsalian16@gmail.com>
2020-06-02 14:44:56 +0530

    changes commit

--- React-The Complete Guide Udemy/Projects/http--01-starting-setup/src/index.js
+++ React-The Complete Guide Udemy/Projects/http--01-starting-setup/src/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
 // axios.defaults.headers.common['Authorization']="AUTH TOKEN";
-axios.defaults.headers.post['Content-type']='application/json';
+axios.defaults.headers.post["Content-type"] = "application/json";
 
-axios.interceptors.request.use(request=>{
+axios.interceptors.request.use(
+  (request) => {
     console.log(request);
     return request;
-},error=>{
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  }
+);
 
-axios.interceptors.response.use(response=>{
+axios.interceptors.response.use(
+  (response) => {
     return response;
-},error=>{
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  }
+);
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
